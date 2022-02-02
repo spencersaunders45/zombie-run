ed2d5756817f46b30e80255d7d370c4d5a15d00c
Spencer Saunders <spencer.saunders45@gmail.com>
2022-02-02 10:40:42 -0600

    Added zombies to spawn and method to healthem

--- Main.java
+++ Main.java
@@ -39,11 +39,12 @@ public class Main {
          {3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3},
          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
       };
-
       // Display the map and player data
+      System.out.print("\n\n\n\n\n\n\n");
       System.out.println("UP(w) DOWN(s) LEFT(a) RIGHT(d)");
-      System.out.println("Health: 100     Round: 1");
-      System.out.println("");
+      System.out.println("Health: 100          Round: 1");
+      System.out.println("Weapon: Pistol");
+      System.out.println();
       for(int i = 0; i < 30; i++){
          for(int j = 0; j < 30; j++){
             if(map[i][j] == 0){
@@ -57,6 +58,56 @@ public class Main {
             }
          }
          System.out.print('\n');
+      }
+   }
+
+
+
+   public static Zombie[] enemies(int level){
+      Zombie[] zomArr = {};
+      if(level == 1){
+         Zombie zombie1 = new Zombie();
+         Zombie zombie2 = new Zombie();
+         Zombie zombie3 = new Zombie();
+         zomArr[0] = zombie1;
+         zomArr[1] = zombie2;
+         zomArr[2] = zombie3;
+      } else if(level == 2){
+         Zombie zombie4 = new Zombie();
+         zomArr[3] = zombie4;
+      } else if(level == 3){
+         Zombie zombie5 = new Zombie();
+         zomArr[4] = zombie5;
+      } else if(level == 4){
+         Zombie zombie6 = new Zombie();
+         zomArr[5] = zombie6;
+      } else if(level == 5){
+         Zombie zombie7 = new Zombie();
+         zomArr[6] = zombie7;
+      } else if(level == 6){
+         Zombie zombie8 = new Zombie();
+         zomArr[7] = zombie8;
+      } else if(level == 7){
+         Zombie zombie9 = new Zombie();
+         zomArr[8] = zombie9;
+      } else if(level == 8){
+         Zombie zombie10 = new Zombie();
+         zomArr[9] = zombie10;
+      } else if(level == 9){
+         Zombie zombie11 = new Zombie();
+         zomArr[10] = zombie11;
+      } else if(level == 10){
+         Zombie zombie12 = new Zombie();
+         zomArr[11] = zombie12;
+      }
+      return zomArr;
+   }
+
+
+
+   public static void healEnemies(Zombie[] zomArr){
+      for(Zombie i : zomArr){
+         i.healZombie();
       }
    }
 
--- Zombie.java
+++ Zombie.java
@@ -2,4 +2,9 @@ public class Zombie {
    int health = 10;
    int speed = 1;
    int attack = 10;
+
+   // Setters
+   public void healZombie(){
+      this.health = 10;
+   }
 }
