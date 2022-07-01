007a7df518f7357d58cd202e71f97100f27f0ffd
Spencer Saunders <spencer.saunders45@gmail.com>
2022-06-30 19:54:42 -0600

    more refactoring

--- Game/Map/Map.java
+++ Game/Map/Map.java
@@ -32,9 +32,55 @@ public class Map {
         return playerStart;
     }
 
-    public int[][] GetZomStart()
+    public int[][] GetZombieStart()
     {
         return new int[][]{zom1Start, zom2Start, zom3Start, zom4Start, zom5Start, zom6Start, zom7Start,
                 zom8Start, zom9Start, zom10Start, zom11Start, zom12Start};
+    }
+
+    private void SetPlayerStart(int[] playerStart)
+    {
+        this.playerStart = playerStart;
+    }
+
+    private void SetZombieStart(int[][] zomStartArray)
+    {
+        this.zom1Start = zomStartArray[0];
+        this.zom2Start = zomStartArray[1];
+        this.zom3Start = zomStartArray[2];
+        this.zom4Start = zomStartArray[3];
+        this.zom5Start = zomStartArray[4];
+        this.zom6Start = zomStartArray[5];
+        this.zom7Start = zomStartArray[6];
+        this.zom8Start = zomStartArray[7];
+        this.zom9Start = zomStartArray[8];
+        this.zom10Start = zomStartArray[9];
+        this.zom11Start = zomStartArray[10];
+        this.zom12Start = zomStartArray[11];
+    }
+
+    private void SetMap(int[][] map)
+    {
+        this.map = map;
+    }
+
+    private void SelectMap(String mapName)
+    {
+        if (mapName == )
+    }
+
+    private void Field()
+    {
+
+    }
+
+    private void City()
+    {
+
+    }
+
+    private void Highway()
+    {
+
     }
 }
